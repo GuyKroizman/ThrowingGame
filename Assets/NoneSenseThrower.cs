786f3790a1eb58ce5ece304a873737c1293c8ef7
GuyKroizman <kroizguy@gmail.com>
2018-07-06 19:10:10 +0300

    make animal spin

--- Assets/NoneSenseThrower.cs
+++ Assets/NoneSenseThrower.cs
@@ -9,14 +9,13 @@ public class NoneSenseThrower : MonoBehaviour {
 
 	void Start () {
         throwCountDownCounter = 2;
-
     }
 	
 	void Update () {
         if(throwCountDownCounter < 0)
         {
             int indexOfThingToThrow = Random.Range(0, thingsToThrow.Length);
-            var position = new Vector3(Random.Range(-6, 6), -2, transform.position.z + 2);
+            var position = new Vector3(Random.Range(-6, 6), -2, transform.position.z + 3);
             var thingy = Instantiate(thingsToThrow[indexOfThingToThrow], position, Random.rotation);
 
             var thingyRigidBody = thingy.GetComponent<Rigidbody>();
@@ -24,6 +23,8 @@ public class NoneSenseThrower : MonoBehaviour {
             if (position.x < 0)
                 direction = -1;
             thingyRigidBody.AddForce(new Vector3(direction * Random.Range(0,4) , 9, 0), ForceMode.Impulse);
+
+            thingyRigidBody.AddTorque(1, 1, Random.Range(5f, 20f), ForceMode.Impulse);
 
             Destroy(thingy, 8);
 
